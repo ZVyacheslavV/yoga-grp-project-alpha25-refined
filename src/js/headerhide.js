d1c3c452235e8f32fe2275a5f6967a58cc22b635
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-05-28 15:45:54 +0300

    finish first refining iteration

--- src/js/headerhide.js
+++ src/js/headerhide.js
@@ -1,6 +1,8 @@
 let lastScrollY = window.scrollY;
 const headerHide = document.querySelector('.page-header');
 const mobileHide = document.querySelector('.mobile-menu');
+const burgerBtnHide = document.querySelector('.burger-btn');
+const closeBtnHide = document.querySelector('.close-btn');
 const navScrollLinks = document.querySelectorAll('.header-nav-link');
 
 window.addEventListener('scroll', () => {
@@ -15,6 +17,8 @@ window.addEventListener('scroll', () => {
   const deltaScroll = Math.abs(currentScrollY - lastScrollY);
   if (deltaScroll > 0) {
     mobileHide.classList.remove('is-open');
+    burgerBtnHide.classList.remove('hide');
+    closeBtnHide.classList.add('hide');
   }
 
   /*   const deltaScroll = Math.abs(currentScrollY - lastScrollY);
@@ -23,4 +27,16 @@ window.addEventListener('scroll', () => {
   } */
 
   lastScrollY = currentScrollY;
+});
+
+burgerBtnHide.addEventListener('click', () => {
+  burgerBtnHide.classList.add('hide');
+  closeBtnHide.classList.add('visible');
+  closeBtnHide.classList.remove('hide');
+});
+
+closeBtnHide.addEventListener('click', () => {
+  burgerBtnHide.classList.remove('hide');
+  closeBtnHide.classList.remove('visible');
+  closeBtnHide.classList.add('hide');
 });
