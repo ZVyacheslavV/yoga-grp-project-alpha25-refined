7ecf4733f4b3fb01eb6c36556bfd6c429ffe2172
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-05-28 15:50:39 +0300

    add some commetns

--- src/js/headerhide.js
+++ src/js/headerhide.js
@@ -5,6 +5,7 @@ const burgerBtnHide = document.querySelector('.burger-btn');
 const closeBtnHide = document.querySelector('.close-btn');
 const navScrollLinks = document.querySelectorAll('.header-nav-link');
 
+//Hide-reveal header on vertical scroll more then 40
 window.addEventListener('scroll', () => {
   const currentScrollY = window.scrollY;
 
@@ -14,27 +15,26 @@ window.addEventListener('scroll', () => {
     headerHide.classList.remove('hide');
   }
 
+  //Hiding mobile menu on scroll, changing button X to burger button
   const deltaScroll = Math.abs(currentScrollY - lastScrollY);
   if (deltaScroll > 0) {
     mobileHide.classList.remove('is-open');
     burgerBtnHide.classList.remove('hide');
+    closeBtnHide.classList.remove('visible');
     closeBtnHide.classList.add('hide');
   }
-
-  /*   const deltaScroll = Math.abs(currentScrollY - lastScrollY);
-  if (deltaScroll > 70) {
-    navScrollLinks.forEach(l => l.classList.remove('active-link'));
-  } */
 
   lastScrollY = currentScrollY;
 });
 
+//Hiding burger button on click, X reveal
 burgerBtnHide.addEventListener('click', () => {
   burgerBtnHide.classList.add('hide');
   closeBtnHide.classList.add('visible');
   closeBtnHide.classList.remove('hide');
 });
 
+//Hiding X button on click, burger button reveal
 closeBtnHide.addEventListener('click', () => {
   burgerBtnHide.classList.remove('hide');
   closeBtnHide.classList.remove('visible');
