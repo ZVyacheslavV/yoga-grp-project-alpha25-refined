9a2d154e6f098829eb59e1977b8cc1931a989dbd
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-05-28 14:13:07 +0300

    add some fixes for mobile menu, header

--- src/js/headerhide.js
+++ src/js/headerhide.js
@@ -1,5 +1,6 @@
 let lastScrollY = window.scrollY;
 const headerHide = document.querySelector('.page-header');
+const mobileHide = document.querySelector('.mobile-menu');
 const navScrollLinks = document.querySelectorAll('.header-nav-link');
 
 window.addEventListener('scroll', () => {
@@ -9,6 +10,11 @@ window.addEventListener('scroll', () => {
     headerHide.classList.add('hide');
   } else {
     headerHide.classList.remove('hide');
+  }
+
+  const deltaScroll = Math.abs(currentScrollY - lastScrollY);
+  if (deltaScroll > 0) {
+    mobileHide.classList.remove('is-open');
   }
 
   /*   const deltaScroll = Math.abs(currentScrollY - lastScrollY);
