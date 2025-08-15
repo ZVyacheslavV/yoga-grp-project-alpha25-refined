ae31cf9549537ed07bbe311811a75d9a1507872e
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-08-15 17:35:01 +0300

    add custom push scripts, change header navlinks underline to online observing

--- src/js/headeractivelink.js
+++ src/js/headeractivelink.js
@@ -1,8 +1,34 @@
 const navLinks = document.querySelectorAll('.header-nav-link');
+const sections = document.querySelectorAll('section');
+
+/* export const resetHeaderLinksUnderlines = () =>
+  navLinks.forEach(l => l.classList.remove('active-link'));
 
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
-    navLinks.forEach(l => l.classList.remove('active-link'));
+    resetHeaderLinksUnderlines();
     link.classList.add('active-link');
   });
-});
+}); */
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Прибираємо active з усіх лінків
+        navLinks.forEach(link => link.classList.remove('active-link'));
+
+        // Додаємо active відповідному лінку
+        const id = entry.target.getAttribute('id');
+        console.log(id);
+
+        document
+          .querySelector(`.header-nav-link[href="#${id}"]`)
+          .classList.add('active-link');
+      }
+    });
+  },
+  { threshold: 0.3 } // 30% секції повинно бути видно
+);
+
+sections.forEach(section => observer.observe(section));
